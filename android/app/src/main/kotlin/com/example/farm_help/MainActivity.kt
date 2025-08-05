84abfc5e851e98f44b07438983d425f7511e08cf
AnujShahdeo2204 <anujshahdeo03@gmail.com>
2025-08-05 22:43:34 +0530

    fifth august

--- android/app/src/main/kotlin/com/example/farm_help/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/farm_help/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.farm_help
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color appBarBg = Color(0xFF1F1F1F);        // Dark gray
+  static const Color backgroundColor = Color(0xFF121212); // Almost black
+}
